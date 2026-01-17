95657c188ed28329b368ab2a3b8e0e112cd076b5
HoMinhDang <minhdang202044@gmail.com>
2026-01-17 18:56:10 +0700

    update config, fix datamodule

--- data/pldatamodule.py
+++ data/pldatamodule.py
@@ -6,48 +6,65 @@ from torch.utils.data import DataLoader
 import os
 
 class CrackDataModule(pl.LightningDataModule):
-    def __init__(self, root_dir, img_size=(256, 256), batch_size=16):
+    def __init__(self, root_dir, img_size=(256, 256), batch_size=16, num_workers=4):
         super().__init__()
-        
         self.root_dir = root_dir
         self.batch_size = batch_size
         self.img_size = img_size
-        
+        self.num_workers = num_workers
+
         mu = [0.51789941, 0.51360926, 0.547762]
         std = [0.1812099, 0.17746663, 0.20386334]
-        self.transform = A.Compose(
-            [
-                A.Resize(height=img_size[0], width=img_size[1]),
-                A.Normalize(mean=mu, std=std, max_pixel_value=255.0),
-                ToTensorV2(),
-            ]
-        )
-    
-    def setup(self, stage = None):
-        if stage == 'fit' or stage is None:
+
+        self.transform = A.Compose([
+            A.Resize(img_size[0], img_size[1]),
+            A.Normalize(mean=mu, std=std),
+            ToTensorV2(),
+        ])
+
+    def setup(self, stage=None):
+        if stage == "fit" or stage is None:
             self.train_dataset = CrackDataset(
-                img_dir=os.path.join(self.root_dir, 'train/IMG'),
-                mask_dir=os.path.join(self.root_dir, 'train/GT'),
-                transform=self.transform
+                os.path.join(self.root_dir, "train/IMG"),
+                os.path.join(self.root_dir, "train/GT"),
+                self.transform,
             )
             self.val_dataset = CrackDataset(
-                img_dir=os.path.join(self.root_dir, 'val/IMG'),
-                mask_dir=os.path.join(self.root_dir, 'val/GT'),
-                transform=self.transform
+                os.path.join(self.root_dir, "val/IMG"),
+                os.path.join(self.root_dir, "val/GT"),
+                self.transform,
             )
-        elif stage == 'test' or stage is None:
+
+        if stage == "test" or stage is None:
             self.test_dataset = CrackDataset(
-                img_dir=os.path.join(self.root_dir, 'test/IMG'),
-                mask_dir=os.path.join(self.root_dir, 'test/GT'),
-                transform=self.transform
+                os.path.join(self.root_dir, "test/IMG"),
+                os.path.join(self.root_dir, "test/GT"),
+                self.transform,
             )
-    
+
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=4)
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.num_workers,
+            pin_memory=True,
+        )
 
     def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=False, num_workers=4)
-    
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            pin_memory=True,
+        )
+
     def test_dataloader(self):
-        return DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=4)
-    
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            pin_memory=True,
+        )
